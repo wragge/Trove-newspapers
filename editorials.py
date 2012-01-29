9a4035ef0e9b12ddbb4aafc6e79d82ff452c83ab
Tim Sherratt <tim@discontents.com.au>
2012-01-29 11:31:27 +1100

    Working on creating a rough harvest of editorials across a year, and then analysing the results. Added several functions.

--- editorials.py
+++ editorials.py
@@ -3,13 +3,19 @@ import datetime
 import calendar
 import os
 import re
+from urllib import quote_plus
+import json
 
 import utilities
-from utilities import parse_date, format_date, find_duplicates, get_titles
+from utilities import parse_date, format_date, find_duplicates, get_titles, clean_filename
+from harvest import TroveNewspapersHarvester
+from titles import get_titles_by_year
+from issues import get_title_issues
 
 HARVEST_DIR = '/Users/tim/Documents/trove/'
 TROVE_URL = 'http://trove.nla.gov.au'
 YAHOO_ID = 'JAp9z33V34HzR4rvRaHUNsRuEadGdaoQlRWYwsObAM1YquTZ.m92jjrhx.X0mOro67op'
+NMA_FOLDER = '/Users/tim/Documents/NMA/1913/editorials/titles'
 
 def check_csv(file_name, year=None, exclude=[]):
     '''
@@ -62,9 +68,142 @@ def check_csv(file_name, year=None, exclude=[]):
             csv_out.writerow({'issue_date': format_date(missing)})
         results.write('</table>\n')
         results.write('</body>\n</html>')
+        
+def check_all_editorials(csv_folder, year):
+    # Get files excluding silly Apple dot files
+    csv_files = [csv for csv in os.listdir(csv_folder) if csv[0] != '.' and csv != 'json']
+    totals = []
+    for csv_file in csv_files:
+        results = check_editorial(os.path.join(NMA_FOLDER, csv_file), year)
+        total_article_dates = results['issues'] - len(results['missing'])
+        print '%s: %s%%' % (results['name'], float(total_article_dates)/results['issues'] * 100)
+        totals.append({'id': results['id'],
+                       'name': results['name'],
+                       'articles': len(results['articles']),
+                       'issues': results['issues'],
+                       'odd': len(results['odd']),
+                       'short': len(results['short']),
+                       'long': len(results['long']),
+                       'missing': len(results['missing']),
+                       'duplicates': len(results['duplicates'])
+                        })
+    json_file = '%s/json/%s-totals.js' % (NMA_FOLDER, year)
+    print json_file
+    with open(json_file, 'wb') as json_data:
+        json.dump(totals, json_data, indent=2)
+    return totals
+        
+    
+def check_editorial(csv_file, year):
+    print csv_file
+    articles = csv.reader(open(csv_file, 'rb'), delimiter=',', quotechar='"')
+    # Things to check:
+    #   Duplicate dates
+    #   Number of words
+    #   Page Number
+    #   Missing -- article for each issue
+    title_id = None
+    title_name = None
+    article_count = 0
+    article_dates = []
+    articles_filtered = []
+    odd_pages = []
+    short_articles = []
+    long_articles = []
+    missing_dates = []
+    for article in articles:
+        article_count += 1
+        if not title_id: title_id = article[5]
+        if not title_name: title_name = article[3]
+        # Get the date
+        try:
+            page_number = int(article[7])
+        except ValueError:
+            page_number = int(re.search(r'(\d+)', article[7]).group(1))
+        article_url = article[2]
+        article_title = article[1]
+        article_date = parse_date(article[6])
+        text = article[10]
+        word_count = len(text.split())
+        # Check if page number is odd or even
+        if page_number % 2: 
+            odd_pages.append({'url': article_url,
+                              'title': article_title,
+                              'date': article_date.isoformat(),
+                              'page': page_number,
+                              'length': word_count})
+        else:
+            if word_count < 100:
+                short_articles.append({'url': article_url,
+                              'title': article_title,
+                              'date': article_date.isoformat(),
+                              'page': page_number,
+                              'length': word_count})
+            elif word_count > 1500:
+                long_articles.append({'url': article_url,
+                              'title': article_title,
+                              'date': article_date.isoformat(),
+                              'page': page_number,
+                              'length': word_count})
+            else:
+                article_dates.append(article_date.isoformat())
+                articles_filtered.append({'url': article_url,
+                              'title': article_title,
+                              'date': article_date.isoformat(),
+                              'page': page_number,
+                              'length': word_count})
+    # Return duplicates
+    duplicate_dates = sorted(set(find_duplicates(article_dates)))
+    # Remove duplicates
+    article_dates = sorted(set(article_dates))
+    issues = get_title_issues(title_id, year)
+    issue_dates = [issue['date'] for issue in issues]
+    for issue_date in issue_dates:
+        if not issue_date in article_dates: missing_dates.append(issue_date)
+    print 'Total articles: %s' % article_count
+    print 'Odd pages: %s' % len(odd_pages)
+    print 'Short articles: %s' % len(short_articles)
+    print 'Long articles: %s' % len(long_articles)
+    print 'Duplicate dates: %s' % len(duplicate_dates)
+    print 'Found articles: %s' % len(articles_filtered)
+    print 'Missing articles: %s' % len(missing_dates)
+    results = {'id': title_id,
+               'name': title_name,
+               'odd': odd_pages,
+               'short': short_articles,
+               'long': long_articles,
+               'articles': articles_filtered,
+               'duplicates': duplicate_dates,
+               'missing': missing_dates,
+               'issues': len(issues)
+               }
+    json_file = '%s/json/%s.js' % (NMA_FOLDER, os.path.basename(csv_file)[:-4])
+    print json_file
+    with open(json_file, 'wb') as json_data:
+        json.dump(results, json_data, indent=2)
+    return results
+
+def harvest_editorials(year, titles=None):
+    if not titles:
+        titles = get_titles_by_year(year)
+    for title in titles:
+        # Remove bracketed details from name for search string
+        query_str = re.search(r'([\w\s\-,\']+)?\(*', title['name']).group(1).strip()
+        clean_name = clean_filename(query_str)
+        filename = '%s/%s-%s.csv' % (NMA_FOLDER, title['id'], clean_name)
+        if not os.path.exists(filename):
+            # construct url
+            url = quote_plus('http://trove.nla.gov.au/newspaper/result?q="%s"&l-textSearchScope=headings+only|scope:headings&fromyyyy=%s&toyyyy=%s&l-title=|%s&l-category=Article|category:Article' % (query_str, year, year, title['id']), '://?=&-,\'')
+            print url
+            # harvest results
+            harvester = TroveNewspapersHarvester()
+            harvester.harvest(url, filename=filename)
 
 class ServerError(Exception):
     pass
         
 if __name__ == "__main__":
     #check_csv('/Users/tim/Documents/NMA/1913/smh-editorials.csv', year=1913, exclude=[6])
+    #harvest_editorials(1913)
+    #check_editorials('/Users/tim/Documents/NMA/1913/editorials/titles/53-Barrier-Miner.csv', 1913)
+    check_all_editorials(NMA_FOLDER, 1913)
